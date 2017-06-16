ba9251901d836c8b0ddc9df61c46c00475c4ccb6
Arianna Chau <ariannachau@yahoo.com>
2017-06-16 16:10:52 -0700

    Add another photo to parallax gallery

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 2400, function(){
+      }, 1800, function(){
         window.location.hash = hash;
       });
     }
