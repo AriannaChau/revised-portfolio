da21990746f69d7b0d56d80b4ecfd379367fda1a
Arianna Chau <ariannachau@yahoo.com>
2017-06-15 23:14:39 -0700

    Add sections and tweak styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,13 @@
 $(document).ready(function(){
-  // Add smooth scrolling to all links
+
+//scrolling//
   $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
       event.preventDefault();
 
-      // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 2400, function(){
@@ -21,12 +17,12 @@ $(document).ready(function(){
   });
   $('#scrollButton').click(function(event){
     event.preventDefault();
-    $('html, body').animate({scrollTop:0}, 900);
+    $('html, body').animate({scrollTop:0}, 1000);
   });
 });
 
 
-// When the user scrolls down 20px from the top of the document, show the button
+//scroll button//
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -36,5 +32,3 @@ function scrollFunction() {
         document.getElementById("scrollButton").style.display = "none";
     }
 }
-
-// When the user clicks on the button, scroll to the top of the document
