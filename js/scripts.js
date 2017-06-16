388fd414eeda045108b4221526fe3ad9a2bccc84
Arianna Chau <ariannachau@yahoo.com>
2017-06-15 22:31:19 -0700

    Add initial files, finish header and scrolling features.

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 2400, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+  $('#scrollButton').click(function(event){
+    event.preventDefault();
+    $('html, body').animate({scrollTop:0}, 900);
+  });
+});
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        document.getElementById("scrollButton").style.display = "block";
+    } else {
+        document.getElementById("scrollButton").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
